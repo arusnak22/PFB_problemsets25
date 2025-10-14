ae4d6b6c5a9190c1cdbe5365b6bb10c9f866d202
Programming for Biology <pfb@info01.cshl.edu>
2025-10-14 11:40:51 -0400

    added f string

--- python1/about_me.py
+++ python1/about_me.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python 3
-print("My name: Anastasia") 
+my_name = "Anastasia"
+favorite_color = "Yellow"
+favorite_activity = "Painting"
+favorite_animal = "Hedgehog"
+print(f'''My name is: {my_name}
+My favorite color: {favorite_color}
+My favorite activity: {favorite_activity}
+My favorite animal: {favorite_animal}''') 
