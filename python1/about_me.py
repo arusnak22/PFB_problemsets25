bc0990d23219c2d2a4230ef5b244cc8daebf5a5c
Programming for Biology <pfb@info01.cshl.edu>
2025-10-14 12:02:17 -0400

    used import sys

--- python1/about_me.py
+++ python1/about_me.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python 3
+#!/usr/bin/env python3
+import sys
 my_name = "Anastasia"
 favorite_color = "Yellow"
 favorite_activity = "Painting"
@@ -6,4 +7,9 @@ favorite_animal = "Hedgehog"
 print(f'''My name is: {my_name}
 My favorite color: {favorite_color}
 My favorite activity: {favorite_activity}
-My favorite animal: {favorite_animal}''') 
+My favorite animal: {favorite_animal}''')
+name = sys.argv[1]
+color = sys.argv[2] 
+animal = sys.argv[3]
+activity = sys.argv[4]
+print('My name is:', name, 'My favorite color:', color, 'My favorite animal:', animal, 'My favorite activity is:', activity)
