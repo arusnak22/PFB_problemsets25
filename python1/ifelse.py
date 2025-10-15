6bae3084d7511565f8c1e7987df0284e847948dc
Programming for Biology <pfb@info01.cshl.edu>
2025-10-15 16:58:57 -0400

    ifelse

---
+++ python1/ifelse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+number = 50
+if number > 0:
+   print('it is positive')
+else:
+   print('number is negative')
+
+if number > 50:
+   print('is greater than 50')
+elif number%2==0:
+   print('it is an even number that is less than 50')
+else:
+   print ('is less than 50')
+
+if number%3==0:
+  print('it is divisible by 3')
+else:
+   print(' is not divisible by 3')
